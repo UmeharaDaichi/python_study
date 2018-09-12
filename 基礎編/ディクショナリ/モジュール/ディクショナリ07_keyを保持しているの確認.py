1a560b18e8e6c03593ea3dc78b9fa368fdf72773
holothuria <umehara.daichi@withone.co.jp>
2018-09-12 21:50:34 +0900

    del は消滅を司り、in は存在を示す

---
+++ 基礎編/ディクショナリ/モジュール/ディクショナリ07_keyを保持しているの確認.py
@@ -0,0 +1,16 @@
+
+
+test_dict_1 = {'YEAR':'2010', 'MONTH':'1', 'DAY':'20'}
+
+print(test_dict_1)
+
+
+print('=================================')
+
+print('YEAR' in test_dict_1)
+print('YEARS' in test_dict_1)
+
+
+
+
+
