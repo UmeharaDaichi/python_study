9b71c94bb33426094657b414e1015480a7b1db28
holothuria <umehara.daichi@withone.co.jp>
2018-09-13 20:47:34 +0900

    凍結されし frozenset

---
+++ 基礎編/セット/モジュール/セット01_セットの基本.py
@@ -0,0 +1,38 @@
+
+
+test_set_1 = {'python', '-', 'izm', '.', 'com'}
+
+print(test_set_1)
+
+
+print('--------------------------------')
+
+for i in test_set_1:
+	print(i)
+
+
+# これはディクショナリ
+test_dict = {}
+
+
+# これはセット
+test_set = {'python'}
+
+# 空のセットは「set」を使う
+empty_set = set()
+
+
+
+print('--------------------------------')
+print('--------------------------------')
+
+test_set_1 = {'python', '-', 'izm', '.', 'com', 'python', 'izm'}
+
+print(test_set_1)
+
+print('--------------------------------')
+
+for i in test_set_1:
+	print(i)
+
+
---
+++ 基礎編/セット/モジュール/セット03_要素の削除.py
@@ -0,0 +1,18 @@
+
+
+
+test_set_1 = {'python', '-', 'izm', '.', 'com'}
+
+test_set_1.remove('-')
+test_set_1.discard('.')
+
+
+print(test_set_1)
+
+
+
+
+
+
+
+
