1a8d1ce800cc91a0c96d1cf6243f893efe5cfe73
holothuria <umehara.daichi@withone.co.jp>
2018-09-14 22:57:31 +0900

    for に便利な range！

---
+++ 基礎編/rangeとxrange/モジュール/rangeとxrange01_rangeの基礎.py
@@ -0,0 +1,23 @@
+
+
+
+for i in range(10):
+	print(i)
+
+
+
+
+print('\n～～～～～～～')
+
+
+for i in range(2, 10):
+	print(i)
+
+
+
+print('\n～～～～～～～')
+
+
+for i in range(-2, 10):
+	print(i)
+
---
+++ 基礎編/rangeとxrange/モジュール/rangeとxrange02_xrangeとは.py
@@ -0,0 +1,15 @@
+
+
+# xrange はpyhon2系でのみ存在。3系ではrangeのみ
+# 3系では、rangeがxrangeに近い形に実装し直されたらしい
+
+# ↓ 2系の場合、xrangeは以下のように書きます(3系では当然エラー)
+
+
+# for i in xrange(10):
+# 	print i
+
+
+
+
+
