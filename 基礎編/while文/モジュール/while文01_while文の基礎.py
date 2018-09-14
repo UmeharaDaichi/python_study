2a20e1599ffd6d590eb037cbcc334b90dcaecac3
holothuria <umehara.daichi@withone.co.jp>
2018-09-14 22:33:15 +0900

    ふつう

---
+++ 基礎編/while文/モジュール/while文01_while文の基礎.py
@@ -0,0 +1,27 @@
+print('\n')
+
+
+counter = 0
+
+while counter < 10:
+	counter += 1
+	print(counter)
+
+
+
+print('\n～～～～～～～～～～～～')
+
+
+counter = 0
+
+
+while True:
+	counter += 1
+	print(counter)
+	if counter == 10:
+		break
+
+
+
+
+
