78f663b6cdf503c1be3066498a938e982148d8cc
holothuria <umehara.daichi@withone.co.jp>
2018-09-17 00:35:41 +0900

    親は()で

---
+++ 応用編/04_クラス継承/モジュール/01_クラス継承の基礎.py
@@ -0,0 +1,39 @@
+
+
+class Country:
+	
+	def __init__(self, country_name):
+		self.country_name = country_name
+	
+	
+# Country を継承した City を作成
+class City(Country):
+
+	def __init__(self, country_name, city_name):
+		super().__init__(country_name)
+		self.city_name = city_name
+
+
+classes = []
+classes.append(City('Japan', 'Tokyo'))
+classes.append(City('USA', 'Washington, D.C.'))
+
+for test_cls in classes:
+	print('===== Class =====')
+	print('country_name --> ' + test_cls.country_name)
+	print('city_name --> ' + test_cls.city_name)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
