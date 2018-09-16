62decbf3d9309ed68239bcf734b2016b6a82715b
holothuria <umehara.daichi@withone.co.jp>
2018-09-17 00:45:16 +0900

    3径には新スタイルしかないので、気にしなくて大丈夫

---
+++ 応用編/05_新旧クラススタイル/モジュール/01_クラススタイルの選択.py
@@ -0,0 +1,25 @@
+
+# Python 3系には、新クラススタイルしかないので、どんな書き方でも新クラススタイルになる
+
+
+# 以下も、3系では新スタイルクラス (Python2系では旧クラススタイルの記述)
+class NewStyleClass:
+	pass
+
+
+
+print(type(NewStyleClass))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
