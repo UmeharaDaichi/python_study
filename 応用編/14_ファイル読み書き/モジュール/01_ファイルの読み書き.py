2b7e8f3f14e1c10063ea21a25d2a0a05cabd0ad9
holothuria <umehara.daichi@withone.co.jp>
2018-09-17 19:51:25 +0900

    code指定しないとSJIS

---
+++ 応用編/14_ファイル読み書き/モジュール/01_ファイルの読み書き.py
@@ -0,0 +1,17 @@
+
+
+
+f = open('read.txt', 'r')
+
+for row in f:
+	print(row)
+
+f.close()
+
+
+
+# ↑何も指定していないこの状態だと、どうやら SJIS が指定されている模様
+
+
+
+
---
+++ 応用編/14_ファイル読み書き/モジュール/02_ファイルの書き込み.py
@@ -0,0 +1,13 @@
+
+f = open('write.txt', 'w')
+
+f.write('Pythonでファイルに書き込みました！')
+
+f.close()
+
+
+# ↑これも SJIS
+
+
+
+
