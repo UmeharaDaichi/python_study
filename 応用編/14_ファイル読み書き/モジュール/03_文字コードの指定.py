962df10a966d94fbc962c2cfd920cd0ef5d10f49
holothuria <umehara.daichi@withone.co.jp>
2018-09-17 19:53:08 +0900

    code指定しないとSJIS

--- 応用編/14_ファイル読み書き/モジュール/03_文字コードの指定.py
+++ 応用編/14_ファイル読み書き/モジュール/03_文字コードの指定.py
@@ -1,16 +1,16 @@
 
 fin_sjis = open('read.txt', 'r', encoding='shift_jis')
-fout_enc = open('enc-jp.txt', 'w', encoding='euc_jp')
+fout_euc = open('euc-jp.txt', 'w', encoding='euc_jp')
 fout_utf = open('utf-8.txt', 'w', encoding='utf-8')
 
 
 for row in fin_sjis:
-	fout_enc.write(row)
+	fout_euc.write(row)
 	fout_utf.write(row)
 
 
 fin_sjis.close()
-fout_enc.close()
+fout_euc.close()
 fout_utf.close()
 
 
