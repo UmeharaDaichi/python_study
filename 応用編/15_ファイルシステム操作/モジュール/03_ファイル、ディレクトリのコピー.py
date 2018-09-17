aa8fa34462164082225955025cbd790dc80a466b
holothuria <umehara.daichi@withone.co.jp>
2018-09-17 21:17:57 +0900

    mkdir！

---
+++ 応用編/15_ファイルシステム操作/モジュール/03_ファイル、ディレクトリのコピー.py
@@ -0,0 +1,22 @@
+
+
+import os
+import shutil
+
+# ファイルのコピー
+shutil.copy('c:/python/src.txt', 'c:/python/dest.txt')
+
+# ディレクトリの再帰的なコピー
+shutil.copytree('c:/python', 'c:/python_backup')
+
+
+
+
+
+
+
+
+
+
+
+
