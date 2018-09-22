36d33db08e045cddb2bd075f7231724c9a36a758
holothuria <umehara.daichi@withone.co.jp>
2018-09-22 16:39:52 +0900

    [::-1](スライス)で逆にするか、reversedで逆にするか

---
+++ 応用編/21_リストの逆順/モジュール/01_リスト自体を逆順にする.py
@@ -0,0 +1,33 @@
+
+
+python_list = []
+python_list.append('python')
+python_list.append('izm')
+python_list.append('sample')
+python_list.append('list')
+python_list.append('reversed')
+
+
+print('---------------------------------')
+print('【そのまま表示】')
+
+for value in python_list:
+	print(value)
+
+
+
+# listの順番を逆にする
+python_list.reverse()
+
+
+print('---------------------------------')
+print('【逆順表示】')
+for value in python_list:
+	print(value)
+
+
+
+
+
+
+
