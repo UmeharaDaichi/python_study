966e8911fedf5404b2b1136c7373b192b2e8d57c
holothuria <umehara.daichi@withone.co.jp>
2018-09-22 16:47:54 +0900

    enumerateでインデックス値と値を同時に取得

---
+++ 応用編/22_インデックス付きループ/モジュール/01_インデックスと値を同時に取り出す.py
@@ -0,0 +1,13 @@
+
+
+
+test_list = ['python', '-', 'izm', '.', 'com']
+
+for idx, val in enumerate(test_list):
+	print(idx, val)
+
+
+
+
+
+
---
+++ 応用編/22_インデックス付きループ/モジュール/02_開始値を指定する場合は.py
@@ -0,0 +1,8 @@
+
+test_list = ['python', '-', 'izm', '.', 'com']
+
+for idx, val in enumerate(test_list, 1):
+	print(idx, val)
+
+# ↑採番を 1 からする場合(無指定なら本来の番号通りに 0 から開始)
+
