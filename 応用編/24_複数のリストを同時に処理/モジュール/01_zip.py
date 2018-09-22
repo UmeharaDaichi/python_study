e1ab83bf32b96dd0a0da6c52f04267432318ebda
holothuria <umehara.daichi@withone.co.jp>
2018-09-23 01:56:40 +0900

    一度に複数のリストを使うにはzip！

---
+++ 応用編/24_複数のリストを同時に処理/モジュール/01_zip.py
@@ -0,0 +1,17 @@
+
+
+item_list = ['desktop', 'laptop', 'tablet', 'smartphone']
+stock_list = [12, 83, 55, 0]
+
+
+for item_name, stock_count in zip(item_list, stock_list):
+	print('{} / {}'.format(item_name, stock_count))
+
+
+
+
+
+
+
+
+
---
+++ 応用編/24_複数のリストを同時に処理/モジュール/02_zip_longest(izip_longest).py
@@ -0,0 +1,29 @@
+
+from itertools import zip_longest
+
+
+item_list = ['desktop', 'laptop', 'tablet', 'smartphone']
+stock_list = [12, 83, 55]
+
+
+for item_name, stock_count in zip(item_list, stock_list):
+	print('{} / {}'.format(item_name, stock_count))
+
+
+print('～～～～～～～～～～～～')
+
+for item_name, stock_count in zip_longest(item_list, stock_list):
+	print('{} / {}'.format(item_name, stock_count))
+
+
+
+
+print('～～～～～～～～～～～～')
+
+for item_name, stock_count in zip_longest(item_list, stock_list, fillvalue='no stock'):
+	print('{} / {}'.format(item_name, stock_count))
+
+
+
+
+
