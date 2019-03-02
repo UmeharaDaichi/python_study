2e0146ef248b001b6fed1d3f9eb6c4dd3bb58dfd
holothuria <umehara.daichi@withone.co.jp>
2019-03-02 17:20:28 +0900

    UAを指定してURLを叩いたり、タグ情報を取得したりできる

---
+++ 応用編/45_HTML解析/モジュール/01_HTML文字列の取得.py
@@ -0,0 +1,14 @@
+
+import urllib.request
+
+
+url = 'http://www.python-izm.com/'
+
+htmldata = urllib.request.urlopen(url)
+print(htmldata.read().decode('UTF-8'))
+
+
+htmldata.close()
+
+
+
---
+++ 応用編/45_HTML解析/モジュール/03_タグ情報の取得.py
@@ -0,0 +1,32 @@
+
+
+import urllib.request
+from html.parser import HTMLParser
+
+
+class TestParser(HTMLParser):
+
+	def handle_starttag(self, tagname, attribute):
+		if tagname.lower() == 'a':
+			for i in attribute:
+				if i[0].lower() == 'href':
+					print(i[1])
+
+
+
+url = 'http://www.python-izm.com/'
+
+htmldata = urllib.request.urlopen(url)
+
+parser = TestParser()
+parser.feed(htmldata.read().decode('UTF-8'))
+
+parser.close()
+htmldata.close()
+
+
+
+
+
+
+
